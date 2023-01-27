dd57ab80d52474625091f7b96c9a64e108d1e8bb
Igone van_der_Kley <igone@vdkley.nl>
2023-01-27 21:33:11 +0100

    tweede commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,31 +9,48 @@
     <link rel="shortcut icon" href="/IMG/favicon.ico" type="image/x-icon">
     <title>PHP PDO CRUD</title>
 </head>
+
 <body>
     <h1>PHP PDO CRUD</h1>
 
     <form action="create.php" method="post">
 
-        <label for="firstname">Voornaam:</label> <br>
+        <label for="firstname">Voornaam:</label><br>
         <input type="text" name="firstname" id="firstname"><br>
 
-        <label for="infix">Infix:</label> <br>
-        <input type="text" name="infix" id="infix"> <br>
+        <label for="infix">Infix:</label><br>
+        <input type="text" name="infix" id="infix"><br>
 
-        <label for="lastname">Achternaam:</label> <br>
-        <input type="text" name="lastname" id="lastname"> <br><br>
+        <label for="lastname">Achternaam:</label><br>
+        <input type="text" name="lastname" id="lastname"><br>
 
-        <label for="haircolor">Haarkleur:</label> <br>
-        <input type="text" name="haircolor" id="haircolor"> <br><br>
+        <label for="haircolor">Haarkleur:</label><br>
+        <input type="text" name="haircolor" id="haircolor"><br>
 
-        <label for="dateofbirth">Geboortedatum:</label> <br>
-        <input type="date" name="dateofbirth" id="dateofbirth"> <br><br>
-        
+        <label for="dateofbirth">Geboortedatum:</label><br>
+        <input type="date" name="dateofbirth" id="dateofbirth"><br>
+
+        <label for="phonenumber">Telefoonnummer:</label><br>
+        <input type="text" name="phonenumber" id="phonenumber"><br>
+
+        <label for="streetname">Straatnaam:</label><br>
+        <input type="text" name="streetname" id="streetname"><br>
+
+        <label for="housenumber">Huisnummer:</label><br>
+        <input type="number" name="housenumber" id="housenumber"><br>
+
+        <label for="city">Woonplaats:</label><br>
+        <input type="text" name="city" id="city"><br>
+
+        <label for="zipcode">postcode:</label><br>
+        <input type="text" name="zipcode" id="zipcode"><br>
+
+        <label for="countryname">Landnaam:</label><br>
+        <input type="text" name="countryname" id="countryname"><br>
+
         <input type="submit" value="Send!">
 
     </form>
-
-
 
 </body>
 </html>
